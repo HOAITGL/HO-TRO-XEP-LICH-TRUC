d58029e9b9156d35edc54c649a66ca1f229699b9
Trương Minh Hoà <minhhoa.gl1982@gmail.com>
2025-06-19 10:32:05 +0700

    Tạo admin từ main app

--- app.py
+++ app.py
@@ -824,9 +824,10 @@ def init_data_if_needed():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        if not User.query.first():
+            print(">>> Chưa có user, tạo admin...")
+            admin = User(name="Quản trị viên", username="admin", password="admin", role="admin", department="Phòng CNTT", position="Bác sĩ")
+            db.session.add(admin)
+            db.session.commit()
     app.run(debug=True)
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
